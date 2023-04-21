10a6eb171b5eddfeba250df5a07728dcc0aa3c98
Lucas Almeida da Silveira <lucas.asilveira.sh@gmail.com>
2023-04-21 18:50:26 -0300

    bug resolved, add new config

--- eslint-config/eslint.js
+++ eslint-config/eslint.js
@@ -1,49 +1,47 @@
 module.exports = {
-    env: {
-        browser: true,
-        es2021: true,
-        node: true
-    },
-    extends: [
-        'eslint:recommended',
-        'standard',
-        'plugin:prettier/recommended',
-      ],
-    overrides: [
-    ],
-    parserOptions: {
-        ecmaFeatures: {
-            jsx: true
-          },
-        ecmaVersion: 'latest',
-        sourceType: 'module'
-    },
-    rules: {
-        'no-var': 'error',
-        'indent': [
-            'warn',
-            'tab'
-        ],
-        'quotes': [
-            'warn',
-            'double'
-        ],
-        'semi': [
-            'warn',
-            'never'
-        ],
-        'camelcase': [
-            'warn'
-        ],
-        'arrow-parens': 'warn',
-        'arrow-spacing': 'warn',
-        'brace-style': 'warn',
-        'comma-dangle': [
-            'error', 
-            'never'
-        ],
-        'function-paren-newline': [
-            'warn', { 'minItems': 4 }
-        ]
-    }
+	env: {
+		browser: true,
+		es2021: true,
+		node: true
+	},
+	extends: [
+		"eslint:recommended"
+	],
+	overrides: [
+	],
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true
+		},
+		ecmaVersion: "latest",
+		sourceType: "module"
+	},
+	rules: {
+		"no-var": "error",
+		"indent": [
+			"warn",
+			"tab"
+		],
+		"quotes": [
+			"warn",
+			"double"
+		],
+		"semi": [
+			"warn",
+			"never"
+		],
+		"camelcase": [
+			"warn"
+		],
+		"arrow-parens": "warn",
+		"arrow-spacing": "warn",
+		"brace-style": "warn",
+		"comma-dangle": [
+			"error", 
+			"never"
+		],
+		"function-paren-newline": [
+			"warn", { "minItems": 4 }
+		]
+	}
 }
